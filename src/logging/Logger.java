ac4b0b02e65fcfae5ae2d794d4979deef7f0cd58
aaldea02 <arsena@knights.ucf.edu>
2023-10-24 18:56:53 -0400

    Arsen Add 1

---
+++ src/logging/Logger.java
@@ -0,0 +1,93 @@
+/**
+ * Logger class for handling various types of logging.
+ * This class provides methods to log messages at different levels of severity.
+ * It can log messages at INFO, DEBUG, ERROR, and WARN levels.
+ */
+public class Logger {
+    
+    // Enum to represent various types of logging levels.
+    public enum LogLevel {
+        INFO, DEBUG, ERROR, WARN
+    }
+
+    // Variable to hold the current log level for the application.
+    private static LogLevel currentLogLevel = LogLevel.INFO;
+
+    /**
+     * Sets the logging level for the application.
+     *
+     * @param level the LogLevel to set
+     */
+    public static void setLogLevel(LogLevel level) {
+        currentLogLevel = level;
+    }
+
+    /**
+     * Logs a message at the INFO level.
+     *
+     * @param message the message to log
+     * @param args optional arguments to format into the message
+     */
+    public static void info(String message, Object... args) {
+        if (currentLogLevel.ordinal() <= LogLevel.INFO.ordinal()) {
+            log("INFO", message, args);
+        }
+    }
+
+    /**
+     * Logs a message at the DEBUG level.
+     *
+     * @param message the message to log
+     * @param args optional arguments to format into the message
+     */
+    public static void debug(String message, Object... args) {
+        if (currentLogLevel.ordinal() <= LogLevel.DEBUG.ordinal()) {
+            log("DEBUG", message, args);
+        }
+    }
+
+    /**
+     * Logs a message at the ERROR level.
+     *
+     * @param message the message to log
+     * @param args optional arguments to format into the message
+     */
+    public static void error(String message, Object... args) {
+        if (currentLogLevel.ordinal() <= LogLevel.ERROR.ordinal()) {
+            log("ERROR", message, args);
+        }
+    }
+
+    /**
+     * Logs a message at the WARN level.
+     *
+     * @param message the message to log
+     * @param args optional arguments to format into the message
+     */
+    public static void warn(String message, Object... args) {
+        if (currentLogLevel.ordinal() <= LogLevel.WARN.ordinal()) {
+            log("WARN", message, args);
+        }
+    }
+
+    /**
+     * Internal method to perform the logging.
+     *
+     * @param level the log level as a String
+     * @param message the message to log
+     * @param args optional arguments to format into the message
+     */
+    private static void log(String level, String message, Object... args) {
+        // Get the current time for the timestamp
+        String timestamp = java.time.LocalDateTime.now().toString();
+        
+        // Format the message with the optional arguments
+        String formattedMessage = String.format(message, args);
+        
+        // Construct the log entry
+        String logEntry = String.format("[%s] [%s]: %s", timestamp, level, formattedMessage);
+        
+        // Output the log entry (For now, we'll just print it. You could extend this to write to a file.)
+        System.out.println(logEntry);
+    }
+}
