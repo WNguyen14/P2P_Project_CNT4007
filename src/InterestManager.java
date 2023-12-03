5a40103b7d972c12b38a4bc1661baf390d8bf3cc
sensen312 <sensen312school@gmail.com>
2023-12-02 21:35:05 -0500

    Uhhhhhhhhh IDKKK

--- src/InterestManager.java
+++ src/InterestManager.java
@@ -4,28 +4,34 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class InterestManager {
-    private final Map<String, Set<Integer>> interestedPeers;
+    private final Map<Integer, Set<Integer>> interestedPeers;
 
     public InterestManager() {
         interestedPeers = new ConcurrentHashMap<>();
     }
 
-    public void addInterestedPeer(String peerId, int pieceIndex) {
+    public void addInterestedPeer(int peerId, int pieceIndex) {
         interestedPeers.computeIfAbsent(peerId, k -> new HashSet<>()).add(pieceIndex);
     }
 
-    public void removeInterestedPeer(String peerId, int pieceIndex) {
-        if (interestedPeers.containsKey(peerId)) {
-            interestedPeers.get(peerId).remove(pieceIndex);
-            if (interestedPeers.get(peerId).isEmpty()) {
-                interestedPeers.remove(peerId);
-            }
-        }
+    public void removeInterestedPeer(int peerId, int pieceIndex) {
+        interestedPeers.computeIfPresent(peerId, (k, v) -> {
+            v.remove(pieceIndex);
+            return v.isEmpty() ? null : v;
+        });
+    }
+
+    public void removeAllInterestedPieces(int peerId) {
+        interestedPeers.remove(peerId);
     }
 
     public boolean hasInterestedPeers(int pieceIndex) {
         return interestedPeers.values().stream().anyMatch(set -> set.contains(pieceIndex));
     }
-    
-    // Add other methods as needed for your implementation...
+
+    // Additional methods can be added here for managing the interest map
+    // Example: Method to check if a peer is interested in any piece
+    public boolean isPeerInterested(int peerId) {
+        return interestedPeers.containsKey(peerId) && !interestedPeers.get(peerId).isEmpty();
+    }
 }
--- src/handshake.java
+++ src/handshake.java
@@ -1,5 +1,6 @@
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 public class handshake {
@@ -7,9 +8,9 @@ public class handshake {
     private static final String HEADER = "P2PFILESHARINGPROJ";
     private static final int ZERO_BITS_LENGTH = 10;
     private static final int PEER_ID_LENGTH = 4;
-    private String peerID;
+    private int peerID; // Changed to int
 
-    public handshake(String peerID) {
+    public handshake(int peerID) {
         this.peerID = peerID;
     }
 
@@ -20,8 +21,8 @@ public class handshake {
             stream.write(HEADER.getBytes(StandardCharsets.UTF_8));
             // then is 10 zero bytes
             stream.write(new byte[ZERO_BITS_LENGTH]);
-            // tAhen last 4 bytes is peer id
-            stream.write(this.peerID.getBytes(StandardCharsets.UTF_8));
+            // then last 4 bytes is peer id
+            stream.write(ByteBuffer.allocate(PEER_ID_LENGTH).putInt(this.peerID).array());
         } catch (IOException e) {
             // This should never happen with ByteArrayOutputStream
             throw new RuntimeException("Error constructing the handshake message", e);
@@ -29,7 +30,7 @@ public class handshake {
         return stream.toByteArray();
     }
 
-    public static String readHandshake(byte[] handshakeMessage) throws IOException {
+    public static int readHandshake(byte[] handshakeMessage) throws IOException {
         if (handshakeMessage.length != HEADER.length() + ZERO_BITS_LENGTH + PEER_ID_LENGTH) {
             throw new IOException("Handshake message is not the correct size.");
         }
@@ -41,26 +42,27 @@ public class handshake {
         }
 
         // Extract the peer ID
-        return new String(handshakeMessage, HEADER.length() + ZERO_BITS_LENGTH, PEER_ID_LENGTH, StandardCharsets.UTF_8);
+        ByteBuffer buffer = ByteBuffer.wrap(handshakeMessage, HEADER.length() + ZERO_BITS_LENGTH, PEER_ID_LENGTH);
+        return buffer.getInt();
     }
 
-    public String getPeerID() {
+    public int getPeerID() {
         return this.peerID;
     }
 
-    public void setPeerID(String peerID) {
+    public void setPeerID(int peerID) {
         this.peerID = peerID;
     }
 
-	public static String getHeader() {
-		return HEADER;
-	}
+    public static String getHeader() {
+        return HEADER;
+    }
 
-	public static int getZeroBitsLength() {
-		return ZERO_BITS_LENGTH;
-	}
+    public static int getZeroBitsLength() {
+        return ZERO_BITS_LENGTH;
+    }
 
-	public static int getPeerIdLength() {
-		return PEER_ID_LENGTH;
-	}	
+    public static int getPeerIdLength() {
+        return PEER_ID_LENGTH;
+    }
 }
